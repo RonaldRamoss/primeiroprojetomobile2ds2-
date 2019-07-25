9b382dc4016b4790f6380d0e7cafae4761f7e6a7
RonaldRamoss <ronaldvalentimramos@gmail.com>
2019-07-26 03:20:23 +0900

    Função de somaa

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,9 @@
-// This is a JavaScript file
+$(document).on("click","#btnsoma",function(){
+var valor1 = $("#valor1").val();
+var valor2 = $("#valor2").val();
+var resul = parseFloat(valor1) + parseFloat(valor2);
+$("#resultado").val(resul);
+
+
+})
 
