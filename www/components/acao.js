74d87011a1233136fd92312867791afb25eb85ec
RonaldRamoss <ronaldvalentimramos@gmail.com>
2019-08-01 10:19:26 +0900

    Calculadora com funcoes de soma e subtracao concluidas

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,9 +1,93 @@
-$(document).on("click","#btnsoma",function(){
-var valor1 = $("#valor1").val();
-var valor2 = $("#valor2").val();
-var resul = parseFloat(valor1) + parseFloat(valor2);
-$("#resultado").val(resul);
+var v1 ,v2 ,op;
 
+$(document).on("click","#nm1",function(){
+ var nm1 = 1;
+ var resu = $("#resultado").val();
+ $("#resultado").val(resu + nm1);
+});
 
+$(document).on("click","#nm2",function(){
+var resu = $("#resultado").val();
+var nm2 =2;
+$("#resultado").val(resu +nm2);
+});
+
+$(document).on("click","#nm3",function(){
+var nm3 = 3;
+var resu = $("#resultado").val();
+$("#resultado").val(resu + nm3);
 })
 
+$(document).on("click","#nm4",function(){
+var nm4 = 4;
+var resu = $("#resultado").val();
+$("#resultado").val(resu + nm4);
+})
+
+$(document).on("click","#nm5",function(){
+var nm5 = 5;
+var resu = $("#resultado").val();
+$("#resultado").val(resu + nm5);
+})
+
+$(document).on("click","#nm6",function(){
+var nm6 = 6;
+var resu = $("#resultado").val();
+$("#resultado").val(resu + nm6);
+
+});
+
+$(document).on("click","#nm7",function(){
+var nm7 =7;
+var resu = $("#resultado").val();
+$("#resultado").val(resu + nm7);
+})
+
+$(document).on("click","#nm8",function(){
+var nm8 = 8;
+var resu = $("#resultado").val();
+$("#resultado").val(resu + nm8);
+})
+
+$(document).on("click","#nm9",function(){
+var nm9 = 9;
+var resu = $("#resultado").val();
+$("#resultado").val(resu + nm9); 
+})
+
+$(document).on("click","#soma",function(){
+
+  v1 = parseFloat($("#resultado").val());
+  $("#resultado").val("");
+  op = "soma";
+  $("#op").text($(this).val());
+});
+
+$(document).on("click","#subtracao",function(){
+v1 = $("#resultado").val();
+$("#resultado").val("");
+op = "subtracao";
+$("#op").text($(this).val());
+
+});
+
+$(document).on("click","#igual",function(){
+  
+    v2 = parseFloat($("#resultado").val());
+
+    if(op =="soma"){
+      $("#resultado").val(v1 + v2);
+    }
+    if(op=="subtracao"){
+      $("#resultado").val(v1 - v2);
+    }
+  
+})
+
+
+
+$(document).on("click","#limpar",function(){
+
+  $("#resultado").val("");
+
+});
